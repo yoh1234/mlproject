9bdb40d55a13280389576ad14ede1347c82cd636
Youngsuk Oh <chzhfflt1149@gmail.com>
2024-08-26 02:08:23 -0400

    Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -11,10 +11,10 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
+        # super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
     
     def __str__(self):
         return self.error_message
-    
+
 
